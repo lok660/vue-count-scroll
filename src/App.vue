ea5debf26cc0463d16113e4ad8367986751e0dd9
lok660 <lokk7991@gmail.com>
2021-04-25 00:29:42 +0800

    feat:first version

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <VueCountScroll :startVal="0"
+                  :endVal="2021"
+                  :duration="2000" />
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import VueCountScroll from './components/vue-count-scroll.vue'
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
